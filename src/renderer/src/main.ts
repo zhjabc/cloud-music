86eb030d96b5622b45f14e4cd7e000d5c8ac8cf2
kk <1026607377@qq.com>
2025-05-19 11:57:51 +0800

    feat: 数据持久化

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -4,7 +4,10 @@ import "@icon-park/vue-next/styles/index.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+
 const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 
 import router from "./router";
 
--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -3,19 +3,29 @@ import { getUserAccount } from "@/api/user";
 import { UserAccount } from "@/types/model/userAccount";
 import { ref } from "vue";
 
-export const useUserInfo = defineStore("song", () => {
-  const accountInfo = ref<{
-    account: UserAccount["account"];
-    profile: UserAccount["profile"];
-  }>();
+export const useUserInfo = defineStore(
+  "userInfo",
+  () => {
+    const accountInfo = ref<{
+      account: UserAccount["account"];
+      profile: UserAccount["profile"];
+    }>();
 
-  const setUserAccountInfo = async () => {
-    const res = await getUserAccount();
-    accountInfo.value = {
-      account: res.account,
-      profile: res.profile,
+    const setUserAccountInfo = async () => {
+      const res = await getUserAccount();
+      accountInfo.value = {
+        account: res.account,
+        profile: res.profile,
+      };
     };
-  };
 
-  return { accountInfo, setUserAccountInfo };
-});
+    return { accountInfo, setUserAccountInfo };
+  },
+  {
+    persist: {
+      key: "userInfo",
+      storage: localStorage,
+      pick: ["accountInfo"],
+    },
+  },
+);
